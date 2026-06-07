99d11bad20da3f4d5bd7bee8a4f59b5569600e16
sriharip316 <sriharip316@outlook.com>
2026-06-07 18:14:26 +0530

    Ignore error from file Close in Read

--- internal/input/input.go
+++ internal/input/input.go
@@ -37,7 +37,7 @@ func (r *Reader) Read() ([]byte, error) {
 		if err != nil {
 			return nil, err
 		}
-		defer f.Close()
+		defer func() { _ = f.Close() }()
 
 		if r.maxBytes > 0 {
 			info, err := f.Stat()
